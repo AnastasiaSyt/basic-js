4f91956843503d17daa178dbb4cb829bae2363d3
Anastasia Sytsevich <drozdushka@gmail.com>
2022-10-13 12:14:44 +0300

    feat: complete carbon-date and count-cats tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function dateSample(sampleActivity) {
+  if ((typeof sampleActivity === 'string') && (+sampleActivity > 0) && (+sampleActivity < 15) && !isNaN(sampleActivity)) {
+    const k = 0.693/HALF_LIFE_PERIOD;
+    const time = Math.ceil(Math.abs((Math.log(sampleActivity/MODERN_ACTIVITY))/k));
+    return time;
+  } 
+  return false;
 }
 
 module.exports = {
